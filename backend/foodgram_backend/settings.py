3d9c6384d6c03c3eedea591cbe416eaa62213044
marisgan <marisgan@gmail.com>
2024-10-01 22:22:42 +0300

    Add Postgresql to settings

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -62,17 +62,16 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
-    # 'production': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': os.getenv('POSTGRES_DB', 'django'),
-    #     'USER': os.getenv('POSTGRES_USER', 'django'),
-    #     'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-    #     'HOST': os.getenv('DB_HOST', ''),
-    #     'PORT': os.getenv('DB_PORT', 5432)
-    # }, 
+    'production': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
+    }, 
 }
-DATABASES['default'] = DATABASES['dev']
-# DATABASES['default'] = DATABASES['dev' if os.getenv('DJANGO_DEBUG') else 'production']
+DATABASES['default'] = DATABASES['dev' if os.getenv('DJANGO_DEBUG') else 'production']
 
 
 AUTH_PASSWORD_VALIDATORS = [
