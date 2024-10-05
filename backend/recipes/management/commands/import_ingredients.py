72b9c90770e3cac26764a095f514368762861b2d
marisgan <153249543+marisgan@users.noreply.github.com>
2024-10-06 00:25:02 +0300

    Update import_ingredients.py

--- backend/recipes/management/commands/import_ingredients.py
+++ backend/recipes/management/commands/import_ingredients.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         file_path = os.path.join(
-            settings.BASE_DIR, '..', 'data', 'ingredients.json')
+            settings.BASE_DIR, 'data', 'ingredients.json')
 
         try:
             with open(file_path, 'r', encoding='utf-8') as file:
